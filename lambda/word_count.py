ee7ef4750da195d58039cb7acba0060a15ee6cf0
Itay Russak <irussak3@gmail.com>
2023-11-14 11:16:45 +0000

    CharacterCount Lamba function

--- lambda/word_count.py
+++ lambda/word_count.py
@@ -1,21 +1,23 @@
 import requests
-
+import os
 
 def send_request(url, method="GET", payload={}, headers={}):
     response = requests.request("GET", url, headers=headers, data=payload)
     return response.text
 
 
-def count_words(response):
+def count_characters(response):
     splited_response = list(response.replace(" ", ""))
-    word_count = len(splited_response)
-    return f"Character count: {word_count}"
+    character_count = len(splited_response)
+    return {"character_count": character_count}
 
 
-def main():
-    response = send_request(url="http://SimpleWebServerALB-388820569.us-east-1.elb.amazonaws.com")
-    return count_words(response=response)
 
+def lambda_handler(event, context):
+    url = os.environ.get("TARGET_URL")
 
-if __name__ == "__main__":
-    print(main())
+    response = send_request(url=url)
+
+    result = count_characters(response=response)
+
+    return result
